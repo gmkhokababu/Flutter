662381e39ec5165f4b8ade67cc83bb2687cc84dc
gmkhokababu <gmkhokababu@gmail.com>
2024-07-06 14:13:54 +0600

    android Data base connection

---
+++ db_project/lib/signup.dart
@@ -0,0 +1,144 @@
+import 'dart:convert';
+
+import 'package:db_project/main.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+List<Student> objectsFromJson(String str) =>
+    List<Student>.from(json.decode(str).map((x) => Student.fromJson(x)));
+
+String objectsTojson(List<Student> data) =>
+    json.encode(List<Student>.from(data.map((x) => x.toJson())));
+
+class Student {
+  int? id;
+  String? name;
+  String? email;
+  String? round;
+  Student(
+      {required this.id,
+      required this.name,
+      required this.email,
+      required this.round});
+  factory Student.fromJson(Map<String, dynamic> json) => Student(
+      id: json["1279883"],
+      name: json["Abu Hossain"],
+      email: json["abu@email.com"],
+      round: json["57"]);
+  Map<String, dynamic> toJson() {
+    return {"id": id, "name": name, "email": email, "round": round};
+  }
+}
+
+class Signup extends StatefulWidget {
+  const Signup({super.key});
+
+  @override
+  State<Signup> createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  TextEditingController _id = TextEditingController();
+  TextEditingController _name = TextEditingController();
+  TextEditingController _email = TextEditingController();
+  TextEditingController _round = TextEditingController();
+  Future<Student> signupStudent() async {
+    Student s = Student(
+        id: int.parse(_id.text),
+        name: _name.text,
+        email: _email.text,
+        round: _round.text);
+    final response =
+        await http.post(Uri.parse('http://172.20.64.1:8080/insert'),
+            body: jsonEncode(
+              s.toJson(),
+            ),
+            headers: {"content-type": "application/json"});
+    if (response.statusCode == 200) {
+      return Student.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception("error");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Signup"),
+      ),
+      body: Form(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: ListView(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(5),
+                child: TextFormField(
+                  controller: _id,
+                  decoration: InputDecoration(
+                    labelText: 'ID',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(5),
+                child: TextFormField(
+                  controller: _name,
+                  decoration: InputDecoration(
+                    labelText: 'Name',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(5),
+                child: TextFormField(
+                  controller: _email,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(5),
+                child: TextFormField(
+                  controller: _round,
+                  decoration: InputDecoration(
+                    labelText: 'Round',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  Student st = await signupStudent();
+                  if (st != null) {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => MyApp()));
+                  } else if (_id.text.length == 0) {
+                    SnackBar snk =
+                        SnackBar(content: Text("Enter Correct Informatin"));
+                    ScaffoldMessenger.of(context).showSnackBar(snk);
+                  }
+                },
+                child: Text("Signup"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
